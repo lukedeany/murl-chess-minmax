5e1df56363dd902892d778ec8de295b81f81dfb0
lukedeany <luke.deany3@gmail.com>
2025-09-24 10:55:37 -0500

    setup initial piece configuartion

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,60 @@ int main()
         board.setPieceAtPosition( Position {i, 2}, white_piece.get()->getID());
         board.setPieceAtPosition( Position {i, 7}, black_piece.get()->getID());
     }
+
+    for (int i {0}; i < 9; i++)
+    {
+        // holders for our pieces
+        std::unique_ptr<Piece<8,8>> black_piece;
+        std::unique_ptr<Piece<8,8>> white_piece;
+
+        // Set piece to the correct thing based on this switch
+        switch (i) {
+            case 1:
+                black_piece = factory.createPiece<Rook<8,8>>(PieceColor::PieceColorBlack);
+                white_piece = factory.createPiece<Rook<8,8>>(PieceColor::PieceColorWhite);
+                break;
+            case 2:
+                black_piece = factory.createPiece<Knight<8,8>>(PieceColor::PieceColorBlack);
+                white_piece = factory.createPiece<Knight<8,8>>(PieceColor::PieceColorWhite);
+                break;
+            case 3:
+                black_piece = factory.createPiece<Bishop<8,8>>(PieceColor::PieceColorBlack);
+                white_piece = factory.createPiece<Bishop<8,8>>(PieceColor::PieceColorWhite);
+                break;
+            case 4:
+                black_piece = factory.createPiece<Queen<8,8>>(PieceColor::PieceColorBlack);
+                white_piece = factory.createPiece<Queen<8,8>>(PieceColor::PieceColorWhite);
+                break;
+            case 5:
+                black_piece = factory.createPiece<King<8,8>>(PieceColor::PieceColorBlack);
+                white_piece = factory.createPiece<King<8,8>>(PieceColor::PieceColorWhite);
+                break;
+            case 6:
+                black_piece = factory.createPiece<Bishop<8,8>>(PieceColor::PieceColorBlack);
+                white_piece = factory.createPiece<Bishop<8,8>>(PieceColor::PieceColorWhite);
+                break;
+            case 7:
+                black_piece = factory.createPiece<Knight<8,8>>(PieceColor::PieceColorBlack);
+                white_piece = factory.createPiece<Knight<8,8>>(PieceColor::PieceColorWhite);
+                break;
+            case 8:
+                black_piece = factory.createPiece<Rook<8,8>>(PieceColor::PieceColorBlack);
+                white_piece = factory.createPiece<Rook<8,8>>(PieceColor::PieceColorWhite);
+                break;
+            default:
+                black_piece = factory.createPiece<Pawn<8,8>>(PieceColor::PieceColorBlack);
+                white_piece = factory.createPiece<Pawn<8,8>>(PieceColor::PieceColorWhite);
+                break;
+        }
+
+        board.setPieceAtPosition( Position {i, 1}, white_piece.get()->getID());
+        board.setPieceAtPosition( Position {i, 8}, black_piece.get()->getID());
+    }
+
+    // Now for every other piece!
+    // maybe add a file format to read in?
+    
     
     std::cout << board.getPieceAtPosition(Position{3,2}) << std::endl;
 
